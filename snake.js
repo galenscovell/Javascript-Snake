197565662d09f89f58354639cb584dd49e56fae5
Galen Scovell <galen.scovell@gmail.com>
2014-10-31 11:57:48 -0700

    Correct Coordinate System
    Prior iteration created coordinates for x based on height and y based on
    width... switched that and now each square has proper coordinates for
    indentification.

--- snake.js
+++ snake.js
@@ -12,16 +12,16 @@ $(document).ready(function() {
     $('#game_board').width(boardWidth);
     $('#game_board').height(boardHeight);
 
-    for (x = 0; x < boardWidth; x += 20) {
-        for (y = 0; y < boardHeight; y += 20) {
+    for (y = 0; y < boardHeight; y += 20) {
+        for (x = 0; x < boardWidth; x += 20) {
             $('#game_board').append('<div class="game_square" id=pos' + (x / 20) + '_' + (y / 20) + '></div>');
         }
     }
 
     $('#' + positionElement).css('background-color', 'rgba(0, 125, 0, 0.5)');
 
-    $('.game_square').click(function() {
-        console.log( $(this).attr('id') );
+    $('.game_square').mouseover(function() {
+        $('p').html( $(this).attr('id') );
     });
 
 });
@@ -31,6 +31,6 @@ $(document).keypress(function(event) {
     console.log(event.which);
     if (event.which == 13) {
         event.preventDefault();
-        $('p').html("PRESS ARROWS TO CHANGE DIRECTION");
+        $('p').html(boardWidth + "," + boardHeight);
     }
 });
