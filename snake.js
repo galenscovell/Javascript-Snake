2081336c4a4ab9c103f30592b6da5084dd3cf7f6
Galen Scovell <galen.scovell@gmail.com>
2014-10-30 20:51:46 -0700

    Complete Rewrite
    Took me the better part of the night, but I managed to make the
    foundation I hoped for: Creates grid dynamically depending on screen
    size, with each block containing cartesian coordinates. : )

--- snake.js
+++ snake.js
@@ -1,12 +1,33 @@
 
-$(document).ready(function() {
 
-    for (var r = 0; r < 78; r++) {
-        $('#inner_container').append('<div class="column"></div>');
+// Global Variables
+var boardWidth = Math.round(screen.width * 0.7 / 10) * 10;
+var boardHeight = Math.round(screen.height * 0.6 / 10) * 10;
+
+
+$(document).ready(function() {
+    console.log(boardWidth, boardHeight);
+
+    $('#game_board').width(boardWidth);
+    $('#game_board').height(boardHeight);
+
+    for (x = 0; x < boardWidth; x += 20) {
+        for (y = 0; y < boardHeight; y += 20) {
+            $('#game_board').append('<div class="game_square" id=' + (x / 20) + ',' + (y / 20) + '></div>');
+        }
+    }  
+
+    $('.game_square').click(function() {
+        console.log( $(this).attr('id') );
+    });
+
+});
+
+
+$(document).keypress(function(event) {
+    console.log(event.which);
+    if (event.which == 13) {
+        event.preventDefault();
+        $('p').html("PRESS ARROWS TO CHANGE DIRECTION");
     }
-    
-    for (var c = 0; c < 54; c++) {
-        $('.column').append('<div class="gamesquare"></div>');
-    } 
-    
 });
