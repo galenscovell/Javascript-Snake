a90fc93bdc3aa2e48462e7b6ac8daf44a1be8661
Galen <galen.scovell@gmail.com>
2016-01-02 17:14:55 -0800

    Code cleanup/update for portfolio

--- snake.js
+++ snake.js
@@ -1,99 +1,106 @@
+/**
+ * Snake dynamic controls
+ * ======================
+ *
+ * @author GalenS <galen.scovell@gmail.com>
+ */
 
 
-function create_board(width, height) {
+// Grid Variables
+var BOARDWIDTH = Math.round($(window).width() * 0.8 / 24) * 24;
+var BOARDHEIGHT = Math.round($(window).height() * 0.7 / 24) * 24;
+
+// Global
+var CELLSIZE = 24;
+var NEXTROW = (BOARDWIDTH / 24);
+var SNAKE = [];
+var SCORE = 0;
+var SPEED = 80;
+
+
+function createBoard() {
     // Create game board depending on window size
-    $('#game_board').width(width);
-    $('#game_board').height(height);
+    $("#game_board").width(BOARDWIDTH);
+    $("#game_board").height(BOARDHEIGHT);
 
     var count = 0;
 
-    for (y = 0; y < (height / 20); y++) {
-        for (x = 0; x < (width / 20); x++) {
-            $('<div class="game_square" id=' + (x + 1 + count) + '></div>').appendTo('#game_board');
+    for (y = 0; y < (BOARDHEIGHT / CELLSIZE); y++) {
+        for (x = 0; x < (BOARDWIDTH / CELLSIZE); x++) {
+            $("<div class='game_square' id=" + (x + 1 + count) + "></div>").appendTo("#game_board");
 
             // Add border class to first iteration of each row
-            if (x == 0) {
-                $('#' + (x + 1 + count)).addClass('border');
-                $('#' + (x + 1 + count)).css({
-                    'background-color': '#262626',
-                    'box-shadow': 'none'
-                });
-            // Add border class to final iteration of each row
-            } else if (x == ((width / 20) - 1)) {
-                $('#' + (x + 1 + count)).addClass('border');
-                $('#' + (x + 1 + count)).css({
-                    'background-color': '#262626',
-                    'box-shadow': 'none'
+            if (x == 0 || (x == ((BOARDWIDTH / CELLSIZE) - 1))) {
+                $("#" + (x + 1 + count)).addClass("border");
+                $("#" + (x + 1 + count)).css({
+                    "background-color": "#f3f3f3",
+                    "box-shadow": "none"
                 });
             }
-
         }
         // Begin next row, append board width to new iteration
         count += NEXTROW;
-        $("<div id=" + (y + 1) + "></div>").css("clear", "both").appendTo('#game_board');
+        $("<div id=" + (y + 1) + "></div>").css("clear", "both").appendTo("#game_board");
     }
 }
 
 
-function create_snake() {
-    // Initialize 3-unit snake at div 122, 121, 120
-    $('#120').addClass('snake');
-    $('#121').addClass('snake');
-    $('#122').addClass('snakehead');
+function createSnake() {
+    // Initialize 3-unit snake at cell 120, 121, 122
+    $("#120").addClass("snake");
+    $("#121").addClass("snake");
+    $("#122").addClass("snakehead");
 
     // Push snake segments to snake array
-    SNAKE.push('120');
-    SNAKE.push('121');
-    SNAKE.push('122');
+    SNAKE.push("120");
+    SNAKE.push("121");
+    SNAKE.push("122");
 }
 
-
-
-function generate_fruit() {
+function generateFruit() {
     // Generate randomly placed fruit on board
     var fruitGenerated = false;
     while (fruitGenerated == false) {
-        var fruitID = '#' + (Math.round(Math.random() * ((boardWidth / 20) * (boardHeight / 20))));
-        if (!$(fruitID).attr('class').match(/snake|snakehead|border|badfruit/)) {
-            $(fruitID).addClass('fruit');
+        var fruitID = '#' + (Math.round(Math.random() * ((BOARDWIDTH / CELLSIZE) * (BOARDHEIGHT / CELLSIZE))));
+        if (!$(fruitID).attr("class").match(/snake|snakehead|border|badfruit/)) {
+            $(fruitID).addClass("fruit");
             fruitGenerated = true;
         } 
     }
     // Generate randomly placed bad fruit on board
     var badFruitGenerated = 0;
     while (badFruitGenerated !== 5) {
-        var badFruitID = '#' + (Math.round(Math.random() * ((boardWidth / 20) * (boardHeight / 20))));
-        if (!$(badFruitID).attr('class').match(/snake|snakehead|border|fruit/)) {
-            $(badFruitID).addClass('badfruit');
+        var badFruitID = '#' + (Math.round(Math.random() * ((BOARDWIDTH / CELLSIZE) * (BOARDHEIGHT / CELLSIZE))));
+        if (!$(badFruitID).attr("class").match(/snake|snakehead|border|fruit/)) {
+            $(badFruitID).addClass("badfruit");
             badFruitGenerated += 1;
         } 
     }
 }
 
-
-function game_play() {
-    var currentMove = '';
+function gamePlay() {
+    var currentMove = "";
 
     var intervalID = setInterval(function() {
-        if (currentMove != '') {
+        if (currentMove != "") {
             move(currentMove);
         }
-    }, 80);
+    }, SPEED);
 
     $(document).keydown(function(event) {
         event.preventDefault();
         switch (event.which) {
             case 39:
-                currentMove = 'right';
+                currentMove = "right";
                 break;
             case 37:
-                currentMove = 'left';
+                currentMove = "left";
                 break;
             case 38:
-                currentMove = 'up';
+                currentMove = "up";
                 break;
             case 40:
-                currentMove = 'down';
+                currentMove = "down";
                 break;
         }
     });
@@ -103,68 +110,74 @@ function game_play() {
         var nextID = 0;
 
         switch(dir) {
-            case 'right':
+            case "right":
                 nextID = parseInt(currentPosition) + 1;
                 break;
-            case 'left':
+            case "left":
                 nextID = parseInt(currentPosition) - 1;
                 break;
-            case 'up':
+            case "up":
                 nextID = (parseInt(currentPosition) - NEXTROW);
                 break;
-            case 'down':
+            case "down":
                 nextID = (parseInt(currentPosition) + NEXTROW);
                 break;
         }
 
         // Fruit-eating event
         var snakeTail = 0;
-        if ($('#' + nextID).hasClass('fruit')) {
+        if ($("#" + nextID).hasClass("fruit")) {
             snakeTail = SNAKE[0];
-            $('.fruit').removeClass('fruit');
-            $('.badfruit').removeClass('badfruit');
+            $(".fruit").removeClass("fruit");
+            $(".badfruit").removeClass("badfruit");
             SCORE += 10;
-            $('p').html("SCORE: " + SCORE + "pts.");
-            generate_fruit();
+            $("#startslate").html("SCORE: " + SCORE + "pts.");
+            generateFruit();
         }
 
         // Bad fruit-eating event
-        if ($('#' + nextID).hasClass('badfruit')) {
-            $('#' + nextID).removeClass('badfruit');
+        if ($("#" + nextID).hasClass("badfruit")) {
+            $("#" + nextID).removeClass("badfruit");
             SCORE -= 10;
-            $('p').html("SCORE: " + SCORE + "pts.");
+            $("#startslate").html("SCORE: " + SCORE + "pts.");
         }
 
         // Snake collision event
-        if ($('#' + nextID).hasClass('snake')) {
-            currentMove = '';
+        if ($("#" + nextID).hasClass("snake")) {
+            currentMove = "";
             clearInterval(intervalID);
-            alert("Collision! End Score: " + SCORE);
-            init_game();
+            $("#startslate").html("Collision! End Score: " + SCORE);
+            setTimeout(function() {
+                initGame();
+            }, 2000);
             return;
         }
 
         // Top/bottom boundary collision
-        if (!$('#' + nextID).hasClass('game_square')) {
-            currentMove = '';
+        if (!$("#" + nextID).hasClass("game_square")) {
+            currentMove = "";
             clearInterval(intervalID);
-            alert("Collision! End Score: " + SCORE);
-            init_game();
+            $("#startslate").html("Collision! End Score: " + SCORE);
+            setTimeout(function() {
+                initGame();
+            }, 2000);
             return;
         }
 
         // Left/right boundary collision
-        if ($('#' + nextID).hasClass('border')) {
-            currentMove = '';
+        if ($("#" + nextID).hasClass("border")) {
+            currentMove = "";
             clearInterval(intervalID);
-            alert("Collision! End Score: " + SCORE);
-            init_game();
+            $("#startslate").html("Collision! End Score: " + SCORE);
+            setTimeout(function() {
+                initGame();
+            }, 2000);
             return;
         }
 
         // Remove old segment positions
         for (var s = 0; s < SNAKE.length; s++) {
-            $('#' + SNAKE[s]).removeClass('snake');
+            $("#" + SNAKE[s]).removeClass("snake");
             SNAKE[s] = SNAKE[s + 1];
         }
 
@@ -175,51 +188,38 @@ function game_play() {
 
         // Add new segment positions
         for (var u = 0; u < SNAKE.length; u++) {
-            $('#' + SNAKE[u]).addClass('snake');
+            $("#" + SNAKE[u]).addClass("snake");
         }
 
         SNAKE[SNAKE.length - 1] = nextID;
-        $("#" + nextID).addClass('snakehead');
-        $("#" + currentPosition).removeClass('snakehead');
+        $("#" + nextID).addClass("snakehead");
+        $("#" + currentPosition).removeClass("snakehead");
     }
 }
 
-
-function init_game() {
+function initGame() {
     SNAKE = [];
     SCORE = 0;
-    $('.snake').removeClass('snake');
-    $('.snakehead').removeClass('snakehead');
-    $('.fruit').removeClass('fruit');
-    $('.badfruit').removeClass('badfruit');
-    $('p').html("SCORE: " + SCORE + "pts.");
+    $(".snake").removeClass("snake");
+    $(".snakehead").removeClass("snakehead");
+    $(".fruit").removeClass("fruit");
+    $(".badfruit").removeClass("badfruit");
+    $("#startslate").html("SCORE: " + SCORE + "pts.");
 
-    create_snake();
-    generate_fruit();
-    game_play();
+    createSnake();
+    generateFruit();
+    gamePlay();
 }
-
 
 $(document).keypress(function(event) {
     if (event.which === 13) {
-        if ($('p').html() == "PRESS ENTER TO START") {
+        if ($("#startslate").html() == "PRESS ENTER TO START") {
             event.preventDefault();
-            init_game();
+            initGame();
         }
     }
 });
 
-
-// Grid Variables
-var boardWidth = Math.round($(window).width() * 0.8 / 20) * 20;
-var boardHeight = Math.round($(window).height() * 0.7 / 20) * 20;
-
-// Global
-var NEXTROW = (boardWidth / 20);
-var SNAKE = [];
-var SCORE = 0;
-
-
 $(document).ready(function() {
-    create_board(boardWidth, boardHeight);
+    createBoard();
 });
